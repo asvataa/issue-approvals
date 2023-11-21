2d167707856e7562302854a4f4435b3e42994d95
asvataa <tnauruzbayev@greencode.pro>
2023-11-21 16:06:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const issueNumber = core.getInput('issue_number', { required: true });
+    const token = core.getInput('github_token', { required: true });
+    const minApprovals = parseInt(core.getInput('min_approvals', { required: true }));
+    const deployedLabel = core.getInput('deployed_label', { required: true });
+
+    const octokit = github.getOctokit(token);
+
+    const { data: issue } = await octokit.rest.issues.get({
+      ...github.context.repo,
+      issue_number: issueNumber,
+    });
+
+    if (issue.labels.some(label => label.name === deployedLabel)) {
+      core.setFailed(`Issue already has the '${deployedLabel}' label.`);
+      return;
+    }
+
+    const comments = await octokit.rest.issues.listComments({
+      ...github.context.repo,
+      issue_number: issueNumber,
+    });
+
+    const assignees = issue.assignees.map(assignee => assignee.login);
+    const approvalWords = ["yes", "ok", "approved"];
+
+    const approvedAssignees = comments.data.filter(comment =>
+      approvalWords.some(word => comment.body.toLowerCase().includes(word))
+    ).map(comment => comment.user.login);
+
+    const approvalCount = assignees.filter(assignee => approvedAssignees.includes(assignee)).length;
+    const allApproved = approvalCount >= minApprovals;
+
+    core.setOutput('all_approved', allApproved);
+    core.setOutput('approval_count', approvalCount);
+  } catch (error) {
+    core.setFailed(`Error in action: ${error.message}`);
+  }
+}
+
+run();
